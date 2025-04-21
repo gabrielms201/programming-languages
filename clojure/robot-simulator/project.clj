535ed1d07a9058c27d11a74df30bf55f64971e4e
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-04-21 15:44:49 -0300

    Remove unused exercise

--- clojure/robot-simulator/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject robot-simulator "0.1.0-SNAPSHOT"
-  :description "robot-simulator exercise."
-  :url "https://github.com/exercism/clojure/tree/main/exercises/practice/robot-simulator"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
