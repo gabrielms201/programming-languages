639c20b91b529c68b418b7e2e3071903d912ac74
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-04 16:03:15 -0300

    Add subtraction

--- clojure/wordy/src/wordy.clj
+++ clojure/wordy/src/wordy.clj
@@ -2,11 +2,26 @@
   (:require [clojure.string :as str]))
 
 
-(defn is-number? [str] (boolean (re-matches #"\d+" str)))
+(defn is-number? [s] (boolean (re-matches #"\d+" s)))
+(defn calculate-number [s] (Integer/parseInt s))
+
+(defn is-sum? [s] (boolean (re-matches #"(\d+) plus (\d+)" s)))
+(defn is-subtraction? [s] (boolean (re-matches #"(\d+) minus (\d+)" s)))
+
+(defn calculate-sum [expr]
+  (let [[_ first-term second-term] (re-find #"(\d+) plus (\d+)" expr)]
+    (+ (calculate-number first-term) (calculate-number second-term))))
+
+(defn calculate-sub [expr]
+  (let [[_ first-term second-term] (re-find #"(\d+) minus (\d+)" expr)]
+    (- (calculate-number first-term) (calculate-number second-term))))
 
 (defn calculate [problem]
   (cond
-    (is-number? problem) (Integer/parseInt problem)
+    (is-number? problem) (calculate-number problem)
+    (is-sum? problem) (calculate-sum problem)
+    (is-subtraction? problem) (calculate-sub problem)
+
     :else (str "Invalid operation for expression: '" problem "'")))
 
 (defn evaluate
