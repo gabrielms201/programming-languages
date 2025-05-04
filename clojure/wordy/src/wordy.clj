c8d9a6db4007fc956271f891e2c08ab8b84084ce
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-04 16:32:21 -0300

    Refact ops

--- clojure/wordy/src/wordy.clj
+++ clojure/wordy/src/wordy.clj
@@ -1,31 +1,29 @@
 (ns wordy
   (:require [clojure.string :as str]))
 
+(def ops {"minus" -
+          "plus" +
+          "divided by" /})
 
 (defn is-number? [s] (boolean (re-matches #"\d+" s)))
-(defn calculate-number [s] (Integer/parseInt s))
+(defn stoi [s] (Integer/parseInt s))
 
-(defn is-sum? [s] (boolean (re-matches #"(\d+) plus (\d+)" s)))
-(defn is-subtraction? [s] (boolean (re-matches #"(\d+) minus (\d+)" s)))
+(defn calculate [expr] 
+  (let [[_ first-term operation second-term] (re-find #"(\d+) (.*) (\d+)" expr)]
+    ((ops operation) (stoi first-term) (stoi second-term))))
 
-(defn calculate-sum [expr]
-  (let [[_ first-term second-term] (re-find #"(\d+) plus (\d+)" expr)]
-    (+ (calculate-number first-term) (calculate-number second-term))))
 
-(defn calculate-sub [expr]
-  (let [[_ first-term second-term] (re-find #"(\d+) minus (\d+)" expr)]
-    (- (calculate-number first-term) (calculate-number second-term))))
-
-(defn calculate [problem]
-  (cond
-    (is-number? problem) (calculate-number problem)
-    (is-sum? problem) (calculate-sum problem)
-    (is-subtraction? problem) (calculate-sub problem)
-
-    :else (str "Invalid operation for expression: '" problem "'")))
-
+;;todo: da pra dar uma melhorada ainda
 (defn evaluate
   "Evaluates a simple math problem"
   [question]
-  (let [[_ problem] (re-find #"What is (.+)\?" question)]
-    (calculate problem)))
+  (some->> question
+    (re-find #"What is (.+)\?")
+    second
+    (#(if (is-number? %) (stoi %) (calculate %)))))
+
+(and 
+(= (evaluate "What is 5 minus 13?") -8)
+(= (evaluate "What is 5 plus 13?") 18)
+(= (evaluate "What is 5?") 5) 
+(= (evaluate "What is 25 divided by 5?") 5))
