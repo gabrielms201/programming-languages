1fc500d6800b4c0d0e7c56b119b12ac209205f17
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-04 18:26:00 -0300

    Allows chain operations

--- clojure/wordy/src/wordy.clj
+++ clojure/wordy/src/wordy.clj
@@ -20,12 +20,25 @@
   (let [[_ first-term operation second-term] (re-find #"(\d+|\-\d+) (.*) (\d+|\-\d+)" expr)]
     ((ops operation) (stoi first-term) (stoi second-term))))
 
+;; (defn calculate [expr]
+;;   (let [[head & tail] (get-form-from-expression expr)]
+;;     (case
+;;      (is-number? head) (calculate-two-term expr)
+;;      (aaa? head) (evaluate-expr (str/join " " (cons (str (calculate-two-term head)) tail))))))
+    
+    
+(defn get-form-from-expression [expr]
+  (rest (re-find #"(.+) (plus|minus|divided by|multiplied by) (.+)" expr)))
+
+(defn is-simple-expression? [expr]
+  (boolean (re-matches #"(\d+|\-\d+) (plus|minus|divided by|multiplied by) (\d+|\-\d+)" expr)))
+
 (defn calculate [expr]
-  (let [[head & tail]  (rest (re-find #"(.+) (plus|minus|divided by|multiplied by) (.+)" expr))]
-    (if (re-matches #"(\d+|\-\d+) (plus|minus|divided by|multiplied by) (\d+|\-\d+)" head)
-      (calculate (str/join " " (cons (str (calculate-two-term head)) tail)))
-      (calculate-two-term expr) 
-      )))
+  (let [[head & tail] (get-form-from-expression expr)]
+    (cond
+      (is-number? head) (calculate-two-term expr)
+      :else (evaluate-expr (str/join " " (cons (str (evaluate-expr head)) tail))))))
+    
 
 (defn evaluate-expr
   "Evaluates a simple expr"
@@ -34,19 +47,13 @@
     (stoi expr)
     (calculate expr)))
 
-
-
-
-  ;;todo: da pra dar uma melhorada ainda
-
-
   (defn evaluate
     "Evaluates a simple math problem"
     [question]
     (some->> question
-             (re-find #"What is (.+)\?")
-             second
-             evaluate-expr))
+        (re-find #"What is (.+)\?")
+        second
+        evaluate-expr))
 
 
   (str
