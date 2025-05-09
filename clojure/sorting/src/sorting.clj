f1ff2e054517705cf5227573ab71973586a93385
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-09 13:17:34 -0300

    Add partition middle

--- clojure/sorting/src/sorting.clj
+++ clojure/sorting/src/sorting.clj
@@ -2,16 +2,28 @@
 
 
 (defn qs-partition-aux
-  [[x & xs :as lst] pivot low high]
-  (if (empty? lst) 
+  [[x & xs :as vec] pivot low high]
+  (if (empty? vec)
     [low pivot high]
     (if (> x pivot)
       (recur xs pivot low (cons x high))
-      (recur xs pivot (cons x low ) high))))
+      (recur xs pivot (cons x low) high))))
 
-(sorting/qs-partition-aux [3 2 1] 0 '() '())
+(defn partition-middle [vec]
+  (when (empty? vec)
+    (throw (IllegalArgumentException. "array must not be empty")))
+    (let [pivot_idx (quot (count vec) 2)
+          vec_without_pivot (concat (subvec vec 0 pivot_idx) (subvec vec (inc pivot_idx)))
+          pivot (nth vec pivot_idx)]
+      (qs-partition-aux vec_without_pivot pivot '() '())))
 
 
 
-(defn sorting
-  [])
+(sorting/partition-middle [1])
+
+
+
+
+
+
+
--- clojure/sorting/test/sorting_test.clj
+++ clojure/sorting/test/sorting_test.clj
@@ -45,11 +45,11 @@
 
 (deftest evaluate_test_11
   (testing "even number of elements"
-    (is (= ['(1 2 3) 3 '(4 5)] (sorting/qs-partition-aux [5 4 3 2 1] 3 [] [])))))
+    (is (= ['(2 3) 3 '(4 5)] (sorting/qs-partition-aux [5 4 3 2] 3 [] [])))))
 
 (deftest evaluate_test_12
   (testing "odd number of elements"
-    (is (= ['(1 2 3 4) 4 '(5 6)] (sorting/qs-partition-aux [6 5 4 3 2 1] 4 [] [])))))
+    (is (= ['(2 3 4) 4 '(5 6)] (sorting/qs-partition-aux [6 5 4 3 2] 4 [] [])))))
 
 (deftest evaluate_test_13
   (testing "negative numbers"
@@ -62,3 +62,67 @@
 (deftest evaluate_test_15
   (testing "reverse sorted input"
     (is (= ['(1 2 3 4 5) 5 '()] (sorting/qs-partition-aux [5 4 3 2 1] 5 [] [])))))
+
+
+;;
+
+(deftest evaluate_test_16
+  (testing "simple half partition"
+    (is (= ['(1) 2 '(3)] (sorting/partition-middle [3 2 1])))))
+
+
+(deftest evaluate_test_17
+  (testing "reject empty list half"
+    (is (thrown-with-msg? IllegalArgumentException #"^array must not be empty$" (sorting/partition-middle [])))))
+
+(deftest evaluate_test_18
+  (testing "pivot is smallest element middle"
+    (is (= ['() 1 '(2 3)] (sorting/partition-middle [3 1 2])))))
+
+(deftest evaluate_test_19
+  (testing "pivot is largest element middle"
+    (is (= '((3 1) 5 ()) (sorting/partition-middle [1 5 3])))))
+
+(deftest evaluate_test_20
+  (testing "pivot in middle ordered middle"
+    (is (= ['(1 2) 3 '(4 5)] (sorting/partition-middle [5 4 3 2 1])))))
+
+(deftest evaluate_test_21
+  (testing "only one element middle"
+    (is (= ['() 1 '()] (sorting/partition-middle [1])))))
+
+(deftest evaluate_test_22
+  (testing "pivot is negative middle"
+    (is (= ['(-3) -1 '(1 0 2)] (sorting/partition-middle [-3 2 -1 0 1])))))
+
+(deftest evaluate_test_23
+  (testing "all elements greater than pivot middle"
+    (is (= ['() 0 '(1 2 3)] (sorting/partition-middle [3 2 0 1])))))
+
+(deftest evaluate_test_24
+  (testing "all elements less than pivot middle"
+    (is (= ['(3 4 2 1) 5 '()] (sorting/partition-middle [1 2 5 4 3])))))
+
+(deftest evaluate_test_25
+  (testing "pivot equal to all elements middle"
+    (is (= ['(2 2) 2 '()] (sorting/partition-middle [2 2 2])))))
+
+(deftest evaluate_test_26
+  (testing "even number of elements middle"
+    (is (= ['(1) 3 '(4 5)] (sorting/partition-middle [5 4 3 1])))))
+
+(deftest evaluate_test_27
+  (testing "odd number of elements middle"
+    (is (= ['(2 3) 4 '(5 6)] (sorting/partition-middle [6 5 4 3 2 ])))))
+
+(deftest evaluate_test_28
+  (testing "negative numbers middle"
+    (is (= ['(-3) -2 '(-1)] (sorting/partition-middle [-3 -2 -1])))))
+
+(deftest evaluate_test_29
+  (testing "positive and negative numbers middle"
+    (is (= ['(-2 -1) 0 '(2 1)] (sorting/partition-middle [1 2 0 -1 -2])))))
+
+(deftest evaluate_test_30
+  (testing "reverse sorted input middle"
+    (is (= ['(1 2) 3 '(4 5)] (sorting/partition-middle [5 4 3 2 1])))))
