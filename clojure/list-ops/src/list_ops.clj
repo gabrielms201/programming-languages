44d2362a69fc8779fd3f2263f420ad08d6309217
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-16 16:01:16 -0300

    Add list ops

--- clojure/list-ops/src/list_ops.clj
+++ clojure/list-ops/src/list_ops.clj
@@ -1,60 +1,53 @@
 (ns list-ops)
-
-(defn append
-  "Given two vectors, it adds all the items in the second vector to the end of the first vector"
-  [coll1 coll2]
-  ;; function body
-  )
-
-(defn concatenate
-  "Given a vector of vectors, it combines all the vectors into one flattened vector"
-  [colls]
-  ;; function body
-  )
-
-(defn select-if
-  "Given a predicate and a vector, it returns the vector of all items for which predicate(item) is true"
-  [pred coll]
-  ;; function body
-  )
-
-(defn length
-  "Given a vector, it returns the number of items within it"
-  [coll]
-  ;; function body
-  )
-
-
-
 (defn foldl
   "Given a function, a vector, and initial accumulator, it folds (reduces) each item into the accumulator from the left"
   [f coll acc]
   (loop [f f
          coll coll
          acc acc]
-    (let [[head & tail] coll] 
+    (let [[head & tail] coll]
       (if (empty? coll) acc
-        (recur f tail (f acc head))))))
-
+          (recur f tail (f acc head))))))
 
 (defn foldr
   "Given a function, a vector, and an initial accumulator, it folds (reduces) each item into the accumulator from the right"
-[f coll acc]
- (loop [f f
-        coll coll
-        acc acc]
-   (let [head (butlast coll) 
-         tail (last coll)]
-     (if (empty? coll) acc
-         (recur f head (f acc tail))))))
+  [f coll acc]
+  (loop [f f
+         coll coll
+         acc acc]
+    (let [head (butlast coll)
+          tail (last coll)]
+      (if (empty? coll) acc
+          (recur f head (f acc tail))))))
+
+(defn append
+  "Given two vectors, it adds all the items in the second vector to the end of the first vector"
+  [coll1 coll2] 
+  (foldl #(conj %1 %2) coll2 coll1))
+
+(defn concatenate
+  "Given a vector of vectors, it combines all the vectors into one flattened vector"
+  [colls]
+  (foldl #(append %1 %2) colls []))
+
+(defn select-if
+  "Given a predicate and a vector, it returns the vector of all items for which predicate(item) is true"
+  [pred coll]
+  (foldl (fn [acc cur] (if (pred cur) (conj acc cur) acc)) coll []))
+
+(defn length
+  "Given a vector, it returns the number of items within it"
+  [coll]
+  (foldr (fn [acc _ ] (inc acc)) coll 0))
 
 (defn apply-to-each
   "Given a function and a vector, it returns the vector of the results of applying function(item) on all items"
   [f coll]
-  (foldr #(conj %1 (f %2)) coll '()))
+  (foldl #(conj %1 (f %2)) coll []))
 
 (defn reverse-order
   "Given a vector, it returns a vector with all the original items, but in reverse order"
   [coll]
-  ;; function body
-  )
+  (foldr #(conj %1 %2) coll []))
+
+(list-ops/concatenate [[1 2] [3] [] [4 5 6]])
