a61c4f3ef3d16a69f55f4e625b7b03e00f05c61a
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-20 14:15:38 -0300

    refact colors

--- clojure/resistor-color/src/resistor_color.clj
+++ clojure/resistor-color/src/resistor_color.clj
@@ -1,19 +1,10 @@
 (ns resistor-color)
 
- (def colors-map
-   (array-map
-    "black" 0
-    "brown" 1
-    "red" 2
-    "orange" 3
-    "yellow" 4
-    "green" 5
-    "blue" 6
-    "violet" 7
-    "grey" 8
-    "white" 9))
+ (def colors 
+   ["black" "brown" "red" "orange" "yellow" "green" "blue" "violet" "grey" "white"])
  
- (def colors (keys colors-map))
+ (def colors-map
+   (zipmap colors (range 0 10)))
 
  (defn color-code
    "Returns the numerical value associated with the given color"
