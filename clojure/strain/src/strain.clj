3a2636b9672ccdb168bc61bc49c52487e48d0e01
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-26 19:07:12 -0300

    Finishes transduce exercisce

--- clojure/strain/src/strain.clj
+++ clojure/strain/src/strain.clj
@@ -1,7 +1,5 @@
 (ns strain)
 
-
-
 (defn my-non-lazy-seq
   [xform coll]
   (loop [[head & tail] coll 
@@ -9,7 +7,6 @@
     (if (nil? head)
       acc
       (recur tail ((xform conj) acc head)))))
-
 
 (defn retain
   "Keeps the items in coll for which (pred item) returns true."
@@ -27,6 +24,14 @@
 
 (defn discard
   "Removes the items in coll for which (pred item) returns true."
-  [pred coll]
-  ;; function body
-  )
+  ([pred]
+    (fn [rf]
+      (fn
+        ([] (rf))
+        ([acc] (rf acc))
+        ([acc input]
+         (if (pred input)
+           acc
+           (rf acc input))))))
+   ([pred coll]
+    (my-non-lazy-seq (discard pred) coll)))
--- clojure/strain/test/strain_test.clj
+++ clojure/strain/test/strain_test.clj
@@ -69,44 +69,68 @@
   (testing "should be able to call arity 2 for - reduce function"
     (is (= 9 (((strain/retain pos?) -) 10 1)))))
 
-;; (deftest discard_test_1
-;;   (testing "discard on empty vector returns empty vector"
-;;     (is (= []
-;;            (strain/discard (fn [_] true)
-;;                            [])))))
+(deftest discard_test_1
+  (testing "discard on empty vector returns empty vector"
+    (is (= []
+           (strain/discard (fn [_] true)
+                           [])))))
 
-;; (deftest discard_test_2
-;;   (testing "discards everything"
-;;     (is (= []
-;;            (strain/discard (fn [_] true)
-;;                            [1 3 5])))))
+(deftest discard_test_2
+  (testing "discards everything"
+    (is (= []
+           (strain/discard (fn [_] true)
+                           [1 3 5])))))
 
-;; (deftest discard_test_3
-;;   (testing "discards nothing"
-;;     (is (= [1 3 5]
-;;            (strain/discard (fn [_] false)
-;;                            [1 3 5])))))
+(deftest discard_test_3
+  (testing "discards nothing"
+    (is (= [1 3 5]
+           (strain/discard (fn [_] false)
+                           [1 3 5])))))
 
-;; (deftest discard_test_4
-;;   (testing "discards first and last"
-;;     (is (= [2]
-;;            (strain/discard odd?
-;;                            [1 2 3])))))
+(deftest discard_test_4
+  (testing "discards first and last"
+    (is (= [2]
+           (strain/discard odd?
+                           [1 2 3])))))
 
-;; (deftest discard_test_5
-;;   (testing "discards neither first nor last"
-;;     (is (= [1 3]
-;;            (strain/discard even?
-;;                            [1 2 3])))))
+(deftest discard_test_5
+  (testing "discards neither first nor last"
+    (is (= [1 3]
+           (strain/discard even?
+                           [1 2 3])))))
 
-;; (deftest discard_test_6
-;;   (testing "discards strings"
-;;     (is (= ["apple" "banana" "cherimoya"]
-;;            (strain/discard (fn [x] (str/starts-with? x "z"))
-;;                            ["apple" "zebra" "banana" "zombies" "cherimoya" "zealot"])))))
+(deftest discard_test_6
+  (testing "discards strings"
+    (is (= ["apple" "banana" "cherimoya"]
+           (strain/discard (fn [x] (str/starts-with? x "z"))
+                           ["apple" "zebra" "banana" "zombies" "cherimoya" "zealot"])))))
 
-;; (deftest discard_test_7
-;;   (testing "discards vectors"
-;;     (is (= [[1 2 3] [2 1 2] [2 2 1]]
-;;            (strain/discard (fn [x] (boolean (some #{5} x)))
-;;                            [[1 2 3] [5 5 5] [5 1 2] [2 1 2] [1 5 2] [2 2 1] [1 2 5]])))))
+(deftest discard_test_7
+  (testing "discards vectors"
+    (is (= [[1 2 3] [2 1 2] [2 2 1]]
+           (strain/discard (fn [x] (boolean (some #{5} x)))
+                           [[1 2 3] [5 5 5] [5 1 2] [2 1 2] [1 5 2] [2 2 1] [1 2 5]])))))
+
+(deftest discard_test_8
+  (testing "should be able to call arity 0 for conj reduce function"
+    (is (= [] (((strain/discard odd?) conj))))))
+
+(deftest discard_test_9
+  (testing "should be able to call arity 1 for conj reduce function"
+    (is (= [1 2 3] (((strain/discard odd?) conj) [1 2 3])))))
+
+(deftest discard_test_10
+  (testing "should be able to call arity 2 for conj reduce function"
+    (is (= [1 2 3 4] (((strain/discard neg?) conj) [1 2 3] 4)))))
+
+(deftest discard_test_11
+  (testing "should be able to call arity 0 for - reduce function"
+    (is (thrown? clojure.lang.ArityException (((strain/discard odd?) -))))))
+
+(deftest discard_test_12
+  (testing "should be able to call arity 1 for - reduce function"
+    (is (= -10 (((strain/discard odd?) -) 10)))))
+
+(deftest discard_test_12
+  (testing "should be able to call arity 2 for - reduce function"
+    (is (= 9 (((strain/discard neg?) -) 10 1)))))
