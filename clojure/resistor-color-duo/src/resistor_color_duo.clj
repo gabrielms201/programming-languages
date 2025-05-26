a678448961d9d89c40cf6ca0d73bf85acbc893e3
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-26 15:30:51 -0300

    Add resistor color duo

---
+++ clojure/resistor-color-duo/src/resistor_color_duo.clj
@@ -0,0 +1,12 @@
+(ns resistor-color-duo)
+
+(def colors
+  ["black" "brown" "red" "orange" "yellow" "green" "blue" "violet" "grey" "white"])
+
+(def colors-map (zipmap colors (range 0 10)))
+
+(defn resistor-value
+  "Returns the resistor value based on the given colors"
+  [[first second]]
+    (-> (str (colors-map first) (colors-map second))
+        (read-string)))
