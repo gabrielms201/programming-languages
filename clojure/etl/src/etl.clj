6b1a8a2c201afa0c8f3566095056f7d28bf5ee84
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-27 14:58:13 -0300

    Add transducer solution

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -8,8 +8,20 @@
      (reduce (fn [acc v2] (assoc acc (str/lower-case v2) key)) acc val))
    {} source))
 
-(defn transform [source] ;;solution using for
+(defn transform-for [source] ;;solution using for
   (let [score-vector (for [[score letters] source
-                           letter letters] 
+                           letter letters]
                        [(str/lower-case letter) score])]
     (into {} score-vector)))
+
+(defn zip-letters [[score letters]]
+  (zipmap (map str/lower-case letters) (repeat score)))
+
+(defn transform [source] ;;solution using for
+  (let [xform (map zip-letters)]
+    (into {} xform source))) ;; easy transduce call
+
+
+
+
+
