6d5d75015cdfe676565f51cfec39c74fd078a9ba
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-05-27 14:16:26 -0300

    Add ETL

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,10 +1,15 @@
 (ns etl
   (:require [clojure.string :as str]))
 
-(defn transform
+(defn transform-reduce ;;solution using reduce
   [source]
   (reduce
    (fn [acc [key val]]
      (reduce (fn [acc v2] (assoc acc (str/lower-case v2) key)) acc val))
-   {} source)
-  )
+   {} source))
+
+(defn transform [source] ;;solution using for
+  (let [score-vector (for [[score letters] source
+                           letter letters] 
+                       [(str/lower-case letter) score])]
+    (into {} score-vector)))
