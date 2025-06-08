d67c395297eb4d7be188665195b2324372390bfd
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2025-06-08 15:38:57 -0300

    Init grains

---
+++ clojure/grains/src/grains.clj
@@ -0,0 +1,13 @@
+(ns grains)
+
+(defn square
+  "Returns the number of grains on the n-th chessboard square."
+  [n]
+  ;; function body
+  )
+
+(defn total
+  "Returns the total number of grains on the chessboard."
+  []
+  ;; function body
+  )
